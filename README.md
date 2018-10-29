746e1bc7ac62d5240bc6a21260fa531f3ca2c0f2
Arturo Mejia <arturomejiamarmol@gmail.com>
2018-10-29 17:00:55 -0400

    Adding step 2 Toolbar + Web Engine

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 Simple Browser to illustrate this [blog post](https://medium.com/p/562250a2291a/)
 
 
-<img src="images/web_engine.gif" height="600em" />
+<img src="images/toolbar_web_engine" height="600em" />
--- app/src/main/java/org/mozilla/simple/browser/MainActivity.kt
+++ app/src/main/java/org/mozilla/simple/browser/MainActivity.kt
@@ -11,19 +11,53 @@ import android.util.AttributeSet
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import mozilla.components.browser.engine.system.SystemEngine
+import mozilla.components.browser.session.Session
+import mozilla.components.browser.session.SessionManager
 import mozilla.components.concept.engine.EngineView
+import mozilla.components.feature.session.SessionFeature
+import mozilla.components.feature.session.SessionUseCases
+import mozilla.components.feature.toolbar.ToolbarFeature
 
 class MainActivity : AppCompatActivity() {
 
     private val engine = SystemEngine(this)
 
+    private lateinit var toolbarFeature: ToolbarFeature
+    private lateinit var sessionFeature: SessionFeature
+    private val sessionManager = SessionManager(engine)
+    private val sessionUseCases = SessionUseCases(sessionManager)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val session = engine.createSession()
-        engineView.render(session)
-        session.loadUrl("https://mozilla.org")
+        sessionFeature = SessionFeature(
+            sessionManager,
+            sessionUseCases,
+            engineView
+        )
+
+        toolbarFeature = ToolbarFeature(
+            toolbar,
+            sessionManager,
+            sessionUseCases.loadUrl
+        )
+
+        //Adding a default session
+        sessionManager.add(Session("https://www.mozilla.org"))
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        toolbarFeature.start()
+        sessionFeature.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        toolbarFeature.stop()
+        sessionFeature.stop()
     }
 
     override fun onCreateView(parent: View?, name: String?, context: Context, attrs: AttributeSet?): View? =
